e376e3e81c64b56605adbb99a95f1072f40c0c19
KonstantinKostyuk <kkostyuk@rus-robots.ru>
2014-11-07 16:56:38 +0400

    ifconfig example comments

--- Ifconfig/Ifconfig.ino
+++ Ifconfig/Ifconfig.ino
@@ -1,12 +1,17 @@
+/*
+Galileo Net Interface example.
+2014 Nov 01 Konstantin Kostyuk
+
+Galileo specific function system() 
+
+This example code is in the public domain. 	 
+ */
+
 void setup() {
 Serial.begin(9600);
 system("ifconfig eth0 192.168.1.110 netmask 255.255.255.0 up &> /dev/ttyGS0"); //Manual setup of eth0 interface
 }
 void loop() {
 system("ifconfig &> /dev/ttyGS0"); // print result to Galileo console
-//system("ps | grep ssh &> /dev/ttyGS0");
-system("ls -l /root &> /dev/ttyGS0");
-//system("date >> /root/date.txt &> /dev/ttyGS0");
-
-delay(3000);
+delay(5000);
 }
